d93df162687b3341107ad5effa61ecc4d6f73069
CaioRuizz <caio.ruiz00@gmail.com>
2023-05-11 13:55:55 -0300

    subindo codigo

--- app/handler.py
+++ app/handler.py
@@ -1,19 +1,55 @@
+import boto3
 import json
-from typing import Dict, Any
+import pymysql
+import os
 
-def lambda_handler(event: Dict[str, Dict[str, Any]], context: Dict[str, Any]):
-    req_body = event.get('body') or {}
-    query_params = event.get('queryStringParameters') or {}
-    req_headers = event.get('headers') or {}
-    path = event.get('path') or '/'
+def lambda_handler(event, context):
+    
+    # Leitura dos dados da requisição
+    token = event['token']
+    
+    # Conexão com o banco de dados
+    secretsmanager = boto3.client('secretsmanager')
+    response = secretsmanager.get_secret_value(SecretId=f'replenish4me-db-password-{os.environ.get("env", "dev")}')
+    db_password = response['SecretString']
+    rds = boto3.client('rds')
+    response = rds.describe_db_instances(DBInstanceIdentifier=f'replenish4medatabase{os.environ.get("env", "dev")}')
+    endpoint = response['DBInstances'][0]['Endpoint']['Address']
+    # Conexão com o banco de dados
+    with pymysql.connect(
+        host=endpoint,
+        user='admin',
+        password=db_password,
+        database='replenish4me'
+    ) as conn:
+    
+        # Verificação da sessão ativa no banco de dados
+        with conn.cursor() as cursor:
+            sql = "SELECT usuario_id FROM SessoesAtivas WHERE id = %s"
+            cursor.execute(sql, (token,))
+            result = cursor.fetchone()
+            
+            if result is None:
+                response = {
+                    "statusCode": 401,
+                    "body": json.dumps({"message": "Sessão inválida"})
+                }
+                return response
+            
+            usuario_id = result[0]
+            
+            # Busca dos leitores relacionados ao usuário
+            sql = "SELECT id FROM Leitores WHERE usuario_id = %s"
+            cursor.execute(sql, (usuario_id,))
+            result = cursor.fetchall()
+            
+            leitores = [row[0] for row in result]
+            
+            conn.commit()
 
+    # Retorno da resposta da função
     response = {
         "statusCode": 200,
-        "body": json.dumps(req_body),
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        }
+        "body": json.dumps({"leitores": leitores})
     }
-    
     return response
